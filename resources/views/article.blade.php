f73619c675c3d3f9cf972a17f9d57860f4a24552
black-code1 <legrandmahamat1@gmail.com>
2022-03-30 08:59:57 +0100

    Relation MAny To Many

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -10,4 +10,10 @@
     @empty
         <span>Aucun commentaire pour ce post.</span>
     @endforelse
+    <hr>
+    @forelse ($post->tags as $tag)
+        <span>{{ $tag->name }}</span>
+    @empty
+        <span>Aucun tags pour ce post.</span>
+    @endforelse
 @endsection
